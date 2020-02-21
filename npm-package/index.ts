b52c6c2951fb076c1c56cc9fd6fd2ab70581c05d
Mark Erikson <mark@isquaredsoftware.com>
2020-02-20 22:30:09 -0500

    Initial TS conversion

---
+++ npm-package/index.ts
@@ -0,0 +1,21 @@
+import { compose } from 'redux'
+
+export { EnhancerOptions } from './types'
+
+export const composeWithDevTools =
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : function() {
+        if (arguments.length === 0) return undefined
+        if (typeof arguments[0] === 'object') return compose
+        return compose.apply(null, (arguments as any) as Function[])
+      }
+
+export const devToolsEnhancer: typeof window['__REDUX_DEVTOOLS_EXTENSION__'] =
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__
+    : function() {
+        return function(noop) {
+          return noop
+        }
+      }
